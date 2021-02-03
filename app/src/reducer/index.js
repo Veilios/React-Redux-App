a4028a5383de7d1942f25c900a7b146572bcc8cd
Alan Mir <alanpaulphotos@gmail.com>
2021-02-02 23:47:27 -0500

    Created empty reducer

---
+++ app/src/reducer/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    film: [],
+    isFetching: false,
+    error: "Oh no... Error Try again"
+};
+
+const Reducer = (state = initialState, action) => {
+    return state;
+} 
